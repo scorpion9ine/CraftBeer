88ff6248557f9911a3c377c76bc4e185ab60f63d
scorpion9ine <mauriciomiguel.edu@gmail.com>
2025-12-08 20:43:06 -0300

    sexto commit antes dos erros que ocorreram

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,13 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'BrewFest 2024',
         short_name: 'BrewFest',
-        description: 'Aplicativo do Festival de Cervejas Artesanais',
+        description: 'Festival de Cervejas Artesanais',
         theme_color: '#3E2723',
         background_color: '#FFF8E1',
         display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
         icons: [
           {
             src: 'icon-192x192.png',
@@ -28,57 +24,11 @@ export default defineConfig({
             src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
           }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 ano
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'gstatic-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              }
-            }
-          },
-          {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'images-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 dias
-              }
-            }
-          }
-        ]
-      },
-      devOptions: {
-        enabled: false, // Desativa em desenvolvimento
-        type: 'module',
-        navigateFallback: 'index.html'
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
     })
   ]
